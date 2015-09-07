608c864a8b3528fa73f26c1914c49d913fe7fbcc
Kai Sun <jack.sun@pingxx.com>
2015-09-07 15:05:31 +0800

    add propertyAnimation

--- app/src/main/java/com/sunkai/animationdemo/MainActivity.java
+++ app/src/main/java/com/sunkai/animationdemo/MainActivity.java
@@ -2,11 +2,18 @@ package com.sunkai.animationdemo;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.lang.annotation.Annotation;
 
 
 public class MainActivity extends Activity {
+    ImageView imageView;
+    AnimationDrawable animationDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +32,28 @@ public class MainActivity extends Activity {
                 startActivity(new Intent(MainActivity.this, PropertyAnimationActivity.class));
             }
         });
+
+        imageView = (ImageView) findViewById(R.id.image_view);
+        imageView.setBackgroundResource(R.drawable.drawable_animation);
+        animationDrawable = (AnimationDrawable) imageView.getBackground();
+
+        findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animationDrawable.stop();
+                animationDrawable.start();
+            }
+        });
+
     }
+//
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+//            animationDrawable.stop();
+//            animationDrawable.start();
+//            return true;
+//        }
+//        return super.onTouchEvent(event);
+//    }
 }
--- app/src/main/java/com/sunkai/animationdemo/PropertyAnimationActivity.java
+++ app/src/main/java/com/sunkai/animationdemo/PropertyAnimationActivity.java
@@ -1,16 +1,96 @@
 package com.sunkai.animationdemo;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.ObjectAnimator;
+import android.animation.TypeEvaluator;
+import android.animation.ValueAnimator;
 import android.app.Activity;
+import android.graphics.Point;
+import android.graphics.PointF;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.widget.RadioGroup;
 
 public class PropertyAnimationActivity extends Activity {
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_property_animation);
+
+        final RadioGroup radioGroup = (RadioGroup) findViewById(R.id.RadioGroup);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        switch (radioGroup.getCheckedRadioButtonId()) {
+                            case R.id.radioButton1:
+                                valueAnimatorTest();
+                                break;
+                            case R.id.radioButton2:
+                                objectAnimatorTestByXML();
+                                break;
+                            case R.id.radioButton3:
+                                objectAnimatorTest();
+                                break;
+
+                        }
+                    }
+                });
+            }
+        });
+
+    }
+
+    private void objectAnimatorTestByXML() {
+        ObjectAnimator animation = ObjectAnimator.ofFloat(findViewById(R.id.animation_view), "scaleX", 1.0f, 2.0f);
+        animation.setDuration(1000);
+        animation.start();
+    }
+
+
+    private void valueAnimatorTest() {
+        i = 0;
+        ValueAnimator valueAnimator = new ValueAnimator();
+        valueAnimator.setDuration(1000);
+        valueAnimator.setObjectValues(new PointF());
+        valueAnimator.setInterpolator(new LinearInterpolator());
+        valueAnimator.setEvaluator(new TypeEvaluator() {
+            @Override
+            public PointF evaluate(float v, Object o, Object t1) {
+                i++;
+                Log.d("TEST", " " + v + ",i: " + i);
+                PointF pointF = new PointF();
+                pointF.x = 200 * v + 100;
+                pointF.y = 0.5f * 200 * v * v + 100;
+                return pointF;
+            }
+        });
+        valueAnimator.start();
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                PointF pointF = (PointF) valueAnimator.getAnimatedValue();
+                findViewById(R.id.animation_view).setX(pointF.x);
+                findViewById(R.id.animation_view).setY(pointF.y);
+            }
+        });
+    }
+
+    private void objectAnimatorTest() {
+        Animator animation = AnimatorInflater.loadAnimator(PropertyAnimationActivity.this, R.animator.object_animation);
+        animation.setTarget(findViewById(R.id.animation_view));
+        animation.start();
     }
 
 }
